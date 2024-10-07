22a8d0cb292d18f21fdbbf3c11b3cfc2014d5f0e
pbruss <pbruss@bu.edu>
2024-10-07 12:07:21 -0400

    Added file description to connect.sh

--- connect.sh
+++ connect.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-
+# CS/DS533 Case Study 2
+# File that performs SSH connection with the remote virtual machine using the private generated with setup_vm.sh
 PORT=22014
 MACHINE=paffenroth-23.dyn.wpi.edu
 
