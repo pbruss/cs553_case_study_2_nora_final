709fd93ccf307bba115230315634058d904d34a5
pbruss <pbruss@bu.edu>
2024-10-07 12:22:12 -0400

    Modified file description for monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# DS/CS533 Case Study 2
 # A script that monitors the uptime situation of the virtual machine hosting NORA
+
 # Configuration
 PORT=22014
 MACHINE="paffenroth-23.dyn.wpi.edu"
